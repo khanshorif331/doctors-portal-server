2222cac32ba83b835ca468568a6d8153d2267994
khanshorif331 <khanshorif331@gmail.com>
2022-05-13 17:14:33 +0600

    added an api tooo get all the services

--- index.js
+++ index.js
@@ -9,18 +9,32 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri =
-	'mongodb+srv://process.env.DB_USER:process.env.DB_PASS@cluster0.m0h6z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m0h6z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	serverApi: ServerApiVersion.v1,
 })
-client.connect(err => {
-	const collection = client.db('test').collection('devices')
-	// perform actions on the collection object
-	client.close()
-})
+
+async function run() {
+	try {
+		await client.connect()
+		console.log('Database Connected')
+		const serviceCollection = client
+			.db('doctors_portal')
+			.collection('services')
+
+		app.get('/service', async (req, res) => {
+			const query = {}
+			const cursor = serviceCollection.find(query)
+			const services = await cursor.toArray()
+			res.send(services)
+		})
+	} finally {
+	}
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
 	res.send('Hello from doctor uncle')
