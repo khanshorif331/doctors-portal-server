6be5e0810ab426549149b0a0e50bdab209ea5465
khanshorif331 <khanshorif331@gmail.com>
2022-05-13 12:44:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send('Hello from doctor uncle')
+})
+
+app.listen(port, () => {
+	console.log('Doctoors app Listening to port', port)
+})
