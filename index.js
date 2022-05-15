9a395207eb820888b5d564ef94e6489793866263
khanshorif331 <khanshorif331@gmail.com>
2022-05-16 02:09:06 +0600

    added put method for getting the users from db

--- index.js
+++ index.js
@@ -27,12 +27,29 @@ async function run() {
 		const bookingCollection = client
 			.db('doctors_portal')
 			.collection('bookings')
+		const userCollection = client.db('doctors_portal').collection('users')
 
 		app.get('/service', async (req, res) => {
 			const query = {}
 			const cursor = serviceCollection.find(query)
 			const services = await cursor.toArray()
 			res.send(services)
+		})
+
+		app.put('/user/:email', async (req, res) => {
+			const email = req.params.email
+			const user = req.body
+			const filter = { email: email }
+			const options = { upsert: true }
+			const updateDoc = {
+				$set: user,
+			}
+			const result = await userCollection.updateOne(
+				filter,
+				updateDoc,
+				options
+			)
+			res.send(result)
 		})
 
 		// this is not the proper way to query
