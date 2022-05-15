216fc261fc3f18309c1865c61a36e7366eff408c
khanshorif331 <khanshorif331@gmail.com>
2022-05-15 20:50:33 +0600

    created anoother rooute to get the booking by user email

--- index.js
+++ index.js
@@ -74,6 +74,13 @@ async function run() {
 		 *
 		 * ****/
 
+		app.get('/booking', async (req, res) => {
+			const patient = req.query.patient
+			const query = { patient: patient }
+			const bookings = await bookingCollection.find(query).toArray()
+			res.send(bookings)
+		})
+
 		app.post('/booking', async (req, res) => {
 			const booking = req.body
 			console.log(booking)
