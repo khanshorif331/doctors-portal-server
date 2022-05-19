681910d516e7ac2f9c332c6af227897c5cd57f98
khanshorif331 <khanshorif331@gmail.com>
2022-05-19 22:32:19 +0600

    route created for payment with booking id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -155,6 +155,13 @@ async function run() {
 			} else {
 				return res.status(403).send({ message: 'Forbidden Access' })
 			}
+		})
+
+		app.get('/booking/:id', verifyJWT, async (req, res) => {
+			const id = req.params.id
+			const query = { _id: ObjectId(id) }
+			const booking = await bookingCollection.findOne(query)
+			res.send(booking)
 		})
 
 		app.post('/booking', async (req, res) => {
